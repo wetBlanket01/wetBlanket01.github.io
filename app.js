d201511b2f205a89b77873cea78b6ef7d0c960c2
wetBlanket01 <103075722+wetBlanket01@users.noreply.github.com>
2023-02-17 22:30:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.first_name}`;
+p.innerText = `${tg.initDataUnsafe.user.first_name}`;
 
 usercard.appendChild(p);
 
